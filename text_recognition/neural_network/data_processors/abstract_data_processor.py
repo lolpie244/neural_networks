Fixed normalization and data processors

text_recognition/neural_network/data_processors/abstract_data_processor.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Tuple
+import numpy as np
 
 from neural_network.utils.cache import Cache
 
@@ -12,6 +13,28 @@ class Normalize(ABC):
 
 class DataProcessor(ABC):
     @abstractmethod
-    def normilize(self):
+    def normilize(self, x_normilize, y_normalize) -> Cache:
         pass
 
+    @classmethod
+    def split_training(cls, cache, training_size, randomize=True) -> Tuple[Cache, Cache]:
+        x, y = cache.x, cache.y
+
+        m = int(x.shape[1] * training_size)
+
+        if randomize:
+            perm = np.random.permutation(x.shape[1])
+            x = x[:, perm]
+            y = y[:, perm]
+
+        training = Cache(
+            x=x[:, :m],
+            y=y[:, :m],
+        )
+
+        test = Cache(
+            x=x[:, m:],
+            y=y[:, m:],
+        )
+
+        return (training, test)

text_recognition/neural_network/data_processors/normalization.py
@@ -1,8 +1,11 @@
+from typing import Any
+
+from scipy.sparse import data
 from .abstract_data_processor import Normalize
 import pandas as pd
 
 
-class StructuredDataNormilize(Normalize):
+class DataFrameNormalize(Normalize):
     def __init__(self, normilize_coefs=None) -> None:
         self.normilize_coefs = normilize_coefs
 
@@ -21,3 +24,21 @@ class StructuredDataNormilize(Normalize):
             new_dataset[column] = (dataset[column] - min) / (max - min)
 
         return new_dataset
+
+
+class OnlyDummyNormalize(Normalize):
+    def normilize(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        if isinstance(dataset, pd.Series):
+            dataset = dataset.to_frame()
+
+        return pd.get_dummies(
+            dataset,
+            columns=[column for column in dataset.columns],
+        )
+
+
+class EmptyNormalize(Normalize):
+    def normilize(self, dataset) -> Any:
+        if isinstance(dataset, pd.Series):
+            dataset = dataset.to_frame()
+        return dataset

text_recognition/neural_network/functions/activations.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Relu(Function):
     def __call__(self, neurons: np.ndarray) -> np.ndarray:
-        return np.max(0, neurons)
+        return np.maximum(0, neurons)
 
     def derivative(self, neurons: np.ndarray) -> np.ndarray:
         return np.where(neurons < 0, 0, 1)

text_recognition/neural_network/functions/errors.py
@@ -3,6 +3,14 @@ import numpy as np
 from neural_network.functions import Function
 
 
+class CrossEntropy(Function):
+    def __call__(self, y_pred, y_real) -> np.ndarray:
+        return -np.sum(y_real * np.log(y_pred) + (1 - y_real) * np.log(1 - y_pred))
+
+    def derivative(self, y_pred: np.ndarray, y_real: np.ndarray) -> np.ndarray:
+        return (y_pred - y_real) / (y_pred * (1 - y_pred))
+
+
 class CategoricalCrossEntropy(Function):
     def __call__(self, y_pred, y_real) -> np.ndarray:
         return -np.sum(y_real * np.log(y_pred))
@@ -21,7 +29,7 @@ class MeanSquared(Function):
 
 class BinaryCrossEntropy(Function):
     def __call__(self, y_pred, y_real) -> np.ndarray:
-        return np.mean(-(y_real * np.log(y_pred) + (1 - y_real) * np.log(1 - y_pred)), axis=1)
+        return -np.mean((y_real * np.log(y_pred) + (1 - y_real) * np.log(1 - y_pred)))
 
     def derivative(self, y_pred, y_real) -> np.ndarray:
-        return (y_pred - y_real) / (y_pred * (1 - y_pred))
+        return -(y_real / y_pred) + (1 - y_real) / (1 - y_pred)

text_recognition/neural_network/layers/clasification_layer.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 
 from .abstract_layer import Layer
@@ -10,8 +11,8 @@ class ClasificationLayer(Layer):
         self.activation_function = activation_function
         self.neurons_count = neurons_count
 
-        self.weights: np.ndarray
-        self.bias: float
+        self.weights: np.ndarray = None
+        self.bias: np.ndarray = None
         self.cache = Cache(
             linear=np.ndarray(0),
             input_data=np.ndarray(0),
@@ -19,16 +20,13 @@ class ClasificationLayer(Layer):
 
     def initialize_weights(self, input_data_count):
         self.weights: np.ndarray = np.random.rand(self.neurons_count, input_data_count)
-        self.bias = np.random.rand()
-
-    def get_linear(self, input_data):
-        return np.dot(self.weights.T, input_data) + self.bias
+        self.bias = np.random.rand(self.neurons_count, 1)
 
     def forward(self, input_data: np.ndarray):
         if self.weights is None:
             self.initialize_weights(input_data.shape[0])
 
-        linear = self.get_linear(input_data)
+        linear = np.dot(self.weights, input_data) + self.bias
 
         self.cache.update(
             linear=linear,